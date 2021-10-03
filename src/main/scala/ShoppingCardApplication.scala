5071c5908f188f9a981d5aa64c26fc14c19b3169
Costas Mantzoukas <kmantzoukas@gmail.com>
2021-10-03 08:06:58 +0100

    Rename function addNProductToCar  to addProductsToCard to better reflect what it does

--- src/main/scala/ShoppingCardApplication.scala
+++ src/main/scala/ShoppingCardApplication.scala
@@ -5,7 +5,7 @@ import scala.::
 
 class ShoppingCardApplication(val card: ShoppingCard) {
 
-  def addNProductToCard(product: Product, numOfProducts: Int): Unit = {
+  def addProductsToCard(product: Product, numOfProducts: Int): Unit = {
     1 to numOfProducts foreach (_ => card.products :+= product)
   }
 }
--- src/test/scala/ShoppingCardApplicationTest.scala
+++ src/test/scala/ShoppingCardApplicationTest.scala
@@ -10,7 +10,6 @@ class ShoppingCardApplicationTest extends AnyFunSpec with GivenWhenThen{
 
  describe("A shopping card application") {
 
-
    it("should allow the addition of new products in the shopping card"){
 
     val shoppingCardApplication = new ShoppingCardApplication(ShoppingCard())
@@ -19,10 +18,9 @@ class ShoppingCardApplicationTest extends AnyFunSpec with GivenWhenThen{
     val product = Product("Dove Soap", 39.99)
 
     When("The user adds 5 Dove Soaps to the shopping cart")
+    shoppingCardApplication.addProductsToCard(product, 5)
 
     Then("The shopping cart should contain 5 Dove Soaps each with a unit price of 39.9")
-    shoppingCardApplication.addNProductToCard(product, 5)
-
     assert(shoppingCardApplication.card.products.size == 5)
     shoppingCardApplication.card.products.foreach(p => assert(p == product))
 
@@ -30,7 +28,7 @@ class ShoppingCardApplicationTest extends AnyFunSpec with GivenWhenThen{
     assert(computeTotal(shoppingCardApplication.card.products) == BigDecimal(199.95))
   }
 
-   def computeTotal(products: Seq[Product]): BigDecimal =
+    def computeTotal(products: Seq[Product]): BigDecimal =
     products.foldLeft(BigDecimal(0.00))((c,p) => c + p.price).setScale(2, RoundingMode.HALF_EVEN)
  }
 }
